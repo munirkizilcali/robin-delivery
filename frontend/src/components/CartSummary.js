29e5ff2ca317299e3160581086ba905f2abd6cb7
munirkizilcali <munir.kizilcali@gmail.com>
2018-07-22 20:07:46 -0400

    order details moved to navbar and modal is shown after order submitted

--- frontend/src/components/CartSummary.js
+++ frontend/src/components/CartSummary.js
@@ -1,25 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Menu, Label, Icon } from "semantic-ui-react";
+import { Menu, Label, Icon, Button, Header, Modal } from "semantic-ui-react";
 
 import { submitOrder } from "../redux/actions/order";
 
 class CartSummary extends React.Component {
+	state = {
+		orderSubmitted: false
+	};
+
 	handleSubmitOrderClick = (user, cart, restaurant) => {
 		this.props
 			.submitOrder(user, cart, restaurant)
-			.then(() => alert("Your Order is submitted"));
+			.then(() => this.setState({ orderSubmitted: true }));
 	};
 
 	render() {
 		return (
-			<Menu.Item active={true}>
+			<Menu.Item active={true} disabled={!(this.props.cart.length !== 0)}>
 				Cart Total: ${" "}
 				{this.props.cart.reduce((acc, b) => acc + b.price * b.count, 0)}{" "}
 				<Label
 					as="button"
 					size="large"
-					color="orange"
+					color={this.props.cart.length !== 0 ? "orange" : "grey"}
 					onClick={() =>
 						this.handleSubmitOrderClick(
 							this.props.user,
@@ -27,10 +31,36 @@ class CartSummary extends React.Component {
 							this.props.restaurant
 						)
 					}
+					disabled={!(this.props.cart.length !== 0)}
 				>
 					<Icon name="dollar" />
 					Place Order
 				</Label>
+				<Modal
+					open={this.state.orderSubmitted}
+					basic
+					size="small"
+					onClose={() => this.setState({ orderSubmitted: false })}
+				>
+					<Header icon="archive" content="Order Submitted." />
+					<Modal.Content>
+						<p>
+							Your order has been successful! You will be notified
+							when your order has been picked up.
+						</p>
+					</Modal.Content>
+					<Modal.Actions>
+						<Button
+							color="green"
+							inverted
+							onClick={() =>
+								this.setState({ orderSubmitted: false })
+							}
+						>
+							<Icon name="checkmark" /> Ok
+						</Button>
+					</Modal.Actions>
+				</Modal>
 			</Menu.Item>
 		);
 	}
