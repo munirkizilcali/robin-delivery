7f25574ed424143e33599831f26bd26ab9f61887
munirkizilcali <munir.kizilcali@gmail.com>
2018-08-04 08:32:22 -0400

    basename bugs fixed

--- frontend/src/redux/history.js
+++ frontend/src/redux/history.js
@@ -1,3 +1,3 @@
 import { createBrowserHistory } from "history";
 
-export const history = createBrowserHistory({ basename: "/robin-history" });
+export const history = createBrowserHistory({ basename: "/robin-delivery" });
